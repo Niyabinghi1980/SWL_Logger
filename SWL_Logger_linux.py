8b429b80e83d52f7adfeb9a2d7e331df9f63d5d1
stduyck <stevenduyck1980@gmail.com>
2024-10-18 10:41:03 +0200

    Addinge names 1 and 2

--- SWL_Logger_linux.py
+++ SWL_Logger_linux.py
@@ -65,7 +65,7 @@ def save_swl_report():
 
     # Add report
     current_session_reports.append(
-        [callsign_1, qth_locator_1, callsign_2, qth_locator_2, date, time, frequency, mode, readability,
+        [callsign_1, name_1, qth_locator_1, callsign_2, name_2, qth_locator_2, date, time, frequency, mode, readability,
          signal_strength, tone, details])
 
     # Empty inputbox for new report
@@ -148,90 +148,90 @@ entry_callsign_1.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Name 1:").grid(row=0, column=0, padx=5, pady=5, sticky="e")
 entry_name_1 = tk.Entry(root)
-entry_name_1.grid(row=0, column=1, padx=5, pady=5)
+entry_name_1.grid(row=1, column=1, padx=5, pady=5)
 entry_name_1.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="QTH Locator 1:").grid(row=1, column=0, padx=5, pady=5, sticky="e")
 entry_qth_locator_1 = tk.Entry(root)
-entry_qth_locator_1.grid(row=1, column=1, padx=5, pady=5)
+entry_qth_locator_1.grid(row=2, column=1, padx=5, pady=5)
 entry_qth_locator_1.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Callsign 2:").grid(row=2, column=0, padx=5, pady=5, sticky="e")
 entry_callsign_2 = tk.Entry(root)
-entry_callsign_2.grid(row=2, column=1, padx=5, pady=5)
+entry_callsign_2.grid(row=3, column=1, padx=5, pady=5)
 entry_callsign_2.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Name 2:").grid(row=0, column=0, padx=5, pady=5, sticky="e")
 entry_name_2 = tk.Entry(root)
-entry_name_2.grid(row=0, column=1, padx=5, pady=5)
+entry_name_2.grid(row=4, column=1, padx=5, pady=5)
 entry_name_2.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="QTH Locator 2:").grid(row=3, column=0, padx=5, pady=5, sticky="e")
 entry_qth_locator_2 = tk.Entry(root)
-entry_qth_locator_2.grid(row=3, column=1, padx=5, pady=5)
+entry_qth_locator_2.grid(row=5, column=1, padx=5, pady=5)
 entry_qth_locator_2.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Date (DD-MM-YYYY):").grid(row=4, column=0, padx=5, pady=5, sticky="e")
 entry_date = tk.Entry(root)
-entry_date.grid(row=4, column=1, padx=5, pady=5)
+entry_date.grid(row=6, column=1, padx=5, pady=5)
 entry_date.insert(0, datetime.now().strftime("%d-%m-%Y"))
 entry_date.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Time (UTC, HH:MM):").grid(row=5, column=0, padx=5, pady=5, sticky="e")
 entry_time = tk.Entry(root)
-entry_time.grid(row=5, column=1, padx=5, pady=5)
+entry_time.grid(row=7, column=1, padx=5, pady=5)
 entry_time.insert(0, datetime.now().strftime("%H:%M"))
 entry_time.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Frequency (MHz):").grid(row=6, column=0, padx=5, pady=5, sticky="e")
 entry_frequency = tk.Entry(root)
-entry_frequency.grid(row=6, column=1, padx=5, pady=5)
+entry_frequency.grid(row=8, column=1, padx=5, pady=5)
 entry_frequency.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Mode:").grid(row=7, column=0, padx=5, pady=5, sticky="e")
 entry_mode = tk.StringVar(root)
 mode_menu = tk.OptionMenu(root, entry_mode, "SSB", "CW", "AM", "FM", "Digital")  # Different modi
-mode_menu.grid(row=7, column=1, padx=5, pady=5)
+mode_menu.grid(row=9, column=1, padx=5, pady=5)
 
 tk.Label(root, text="Readability:").grid(row=8, column=0, padx=5, pady=5, sticky="e")
 entry_readability = tk.Entry(root)
-entry_readability.grid(row=8, column=1, padx=5, pady=5)
+entry_readability.grid(row=10, column=1, padx=5, pady=5)
 entry_readability.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Signal strength:").grid(row=9, column=0, padx=5, pady=5, sticky="e")
 entry_signalstrength = tk.Entry(root)
-entry_signalstrength.grid(row=9, column=1, padx=5, pady=5)
+entry_signalstrength.grid(row=11, column=1, padx=5, pady=5)
 entry_signalstrength.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Tone:").grid(row=10, column=0, padx=5, pady=5, sticky="e")
 entry_tone = tk.Entry(root)
-entry_tone.grid(row=10, column=1, padx=5, pady=5)
+entry_tone.grid(row=12, column=1, padx=5, pady=5)
 entry_tone.bind("<Return>", next_field)  # Connect Enter
 
 tk.Label(root, text="Details:").grid(row=11, column=0, padx=5, pady=5, sticky="e")
 entry_details = tk.Entry(root)
-entry_details.grid(row=11, column=1, padx=5, pady=5)
+entry_details.grid(row=13, column=1, padx=5, pady=5)
 entry_details.bind("<Return>", next_field)  # Connect Enter
 
 # Listbox for reports
 report_list = tk.Listbox(root, height=10, width=70)
-report_list.grid(row=12, column=0, columnspan=2, padx=10, pady=5)
+report_list.grid(row=14, column=0, columnspan=2, padx=10, pady=5)
 
 # Save button
 save_button = tk.Button(root, text="Save", command=save_swl_report)
-save_button.grid(row=13, column=0, padx=5, pady=5, sticky="ew")
+save_button.grid(row=15, column=0, padx=5, pady=5, sticky="ew")
 
 # Open button
 open_button = tk.Button(root, text="Open logfile", command=open_excel_file)
-open_button.grid(row=13, column=1, padx=5, pady=5, sticky="ew")
+open_button.grid(row=15, column=1, padx=5, pady=5, sticky="ew")
 
 # About button
 about_button = tk.Button(root, text="About", command=show_about)
-about_button.grid(row=14, column=0, padx=5, pady=5, sticky="ew")
+about_button.grid(row=16, column=0, padx=5, pady=5, sticky="ew")
 
 # Exit button
 exit_button = tk.Button(root, text="Exit", command=root.quit)
-exit_button.grid(row=14, column=1, padx=5, pady=5, sticky="ew")
+exit_button.grid(row=16, column=1, padx=5, pady=5, sticky="ew")
 
 
 # Center buttons
